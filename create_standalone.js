507a360b55f76318fb25edf63e632e4c78185384
Per <per@stuffmatic.com>
2021-10-29 16:36:47 +0200

    rearrange files

--- create_standalone.js
+++ create_standalone.js
@@ -1,4 +1,4 @@
 const fs = require("fs")
 
-fs.writeFileSync("lib/index_standalone.js", fs.readFileSync("lib/index.js").toString().replace(/export /g, ""))
-console.log("Wrote modified lib/index_standalone.js")
+fs.writeFileSync("lib/audio_worklet_starter.js", fs.readFileSync("lib/index.js").toString().replace(/export /g, ""))
+console.log("Wrote modified lib/audio_worklet_starter.js")
